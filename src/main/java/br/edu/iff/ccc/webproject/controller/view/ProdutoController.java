049929759068ee1edb2a0576571a8d8bcadbf8eb
Ronaldo Amaral <ronaldo.amaral@gsuite.iff.edu.br>
2026-08-24 22:26:14 -0300

    Incluindo validação campos DTO

--- src/main/java/br/edu/iff/ccc/webproject/controller/view/ProdutoController.java
+++ src/main/java/br/edu/iff/ccc/webproject/controller/view/ProdutoController.java
@@ -2,12 +2,15 @@ package br.edu.iff.ccc.webproject.controller.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.edu.iff.ccc.webproject.dto.ProdutoRequest;
 import br.edu.iff.ccc.webproject.services.ProdutoUserCase;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/produto")
@@ -23,12 +26,22 @@ public class ProdutoController {
     public String novoProduto(Model model) {
         ProdutoRequest novoProduto = new ProdutoRequest();
         // A chave "produto" é o que usaremos no th:object do Thymeleaf
-        model.addAttribute("produto", novoProduto); 
+        model.addAttribute("produtoRequest", novoProduto); 
         return "produtoForm"; // Retorna templates/produtoForm.html
     }
     
+    
+    // Quando o formulário é enviado, os dados são mapeados para o objeto ProdutoRequest, por ter o mesmo nome de atributos do formulário
+    // O @ModelAttribute é usado para vincular o objeto ProdutoRequest ao formulário, caso o nome do parâmetro seja diferente do nome do objeto no formulário, podemos usar @ModelAttribute("produto") ProdutoRequest produtoRequest
+    // O @Valid é usado para ativar a validação do objeto ProdutoRequest
+    // O BindingResult é usado para capturar os erros de validação
+
     @PostMapping
-    public String criarProduto(ProdutoRequest produtoRequest) {
+    public String criarProduto(@Valid ProdutoRequest produtoRequest, BindingResult result) {
+        if (result.hasErrors()) {
+            // Se houver erros de validação, retorna para o formulário com os erros
+            return "produtoForm";
+        }
         this.produtoUserCase.criarProduto(produtoRequest);
         // Redireciona para a listagem após salvar, evitando reenvio do formulário
         return "redirect:/produto"; 
--- src/main/java/br/edu/iff/ccc/webproject/dto/ProdutoRequest.java
+++ src/main/java/br/edu/iff/ccc/webproject/dto/ProdutoRequest.java
@@ -1,8 +1,16 @@
 package br.edu.iff.ccc.webproject.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class ProdutoRequest {
 
+    @NotBlank(message = "O nome do produto é obrigatório.")
+    @Size(min = 3, max = 100, message = "O nome deve conter entre 3 e 100 caracteres.")
     private String nome;
+
+    @NotBlank(message = "A descrição é obrigatória.")
+    @Size(min = 5, max = 255, message = "A descrição deve conter entre 5 e 255 caracteres.")
     private String descricao;
 
     public ProdutoRequest(String nome, String descricao) {
